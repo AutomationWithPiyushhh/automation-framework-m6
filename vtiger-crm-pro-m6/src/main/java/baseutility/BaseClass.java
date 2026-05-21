3ffb6183c8dbf2e57867805c3bd98f90f3499a7f
AutomationWithPiyushhh <vasudevahir21@gmail.com>
2026-05-21 18:25:29 +0530

    framework exp

--- vtiger-crm-pro-m6/src/main/java/baseutility/BaseClass.java
+++ vtiger-crm-pro-m6/src/main/java/baseutility/BaseClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -20,7 +21,8 @@ public class BaseClass {
 	
 	@BeforeClass
 	public void openBro() {
-		driver = new ChromeDriver(); // Test steps
+		driver = new EdgeDriver(); // Test steps
+		
 		sdriver = driver;
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
